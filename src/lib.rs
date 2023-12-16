123e7bef644bd9a40823ca78c3227d094e64df9f
Thibault Delattre <thibo.delattre@gmail.com>
2023-12-16 14:41:35 +0100

    add comparison types

--- src/lib.rs
+++ src/lib.rs
@@ -1,119 +1,134 @@
+use serde::Deserialize;
+use serde_json::Value;
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
-use serde_json::Value;
-use serde::Deserialize;
-use std::collections::HashMap;
+
+enum ComparisonTypes {
+    Bit,
+    Eq,
+    Gte,
+    Gt,
+    Lte,
+    Lt,
+    Wbit,
+    Weq,
+    Wgt,
+    Wgte,
+    Wlte,
+    Wlt
+}
 
 #[derive(Deserialize, Debug)]
-struct Config {
+pub struct Config {
     name: Option<String>,
     autostart: Option<Autostart>,
     igt: Option<InGameTime>,
     #[serde(default = "HashMap::new")]
     alias: HashMap<String, Value>,
-    definitions: Vec<Split>
+    definitions: Vec<SplitDefinition>,
 }
 
 #[derive(Deserialize, Debug)]
-struct Autostart {
+pub struct Autostart {
     active: String,
     address: String,
     value: String,
     r#type: String,
-    next:  Option<Vec<Split>>,
-    more:  Option<Vec<Split>>,
+    next: Option<Vec<SplitDefinition>>,
+    more: Option<Vec<SplitDefinition>>,
+}
+
+impl Autostart {
+    pub fn is_active(self) -> bool {
+        if self.active == "1" {
+            return true;
+        }
+        false
+    }
 }
 
 #[derive(Deserialize, Debug)]
-struct Split {
+#[serde(rename_all = "camelCase")]
+pub struct SplitDefinition {
     name: Option<String>,
     note: Option<String>,
     address: String,
     value: String,
     r#type: String,
-    next: Option<Vec<Split>>,
-    more: Option<Vec<Split>>,
-
+    next: Option<Vec<SplitDefinition>>,
+    more: Option<Vec<SplitDefinition>>,
 }
 
 #[derive(Deserialize, Debug)]
-struct InGameTime {
+#[serde(rename_all = "camelCase")]
+pub struct InGameTime {
     active: String,
-    framesAddress: String,
-    secondsAddress: String,
-    minutesAddress: String,
-    hoursAddress: String,
+    frames_address: String,
+    seconds_address: String,
+    minutes_address: String,
+    hours_address: String,
 }
 
-pub fn is_active(entry: &String) -> bool {
-    if entry == "1" {
-        return true;
+impl InGameTime {
+    pub fn is_active(self) -> bool {
+        if self.active == "1" {
+            return true;
+        }
+        false
     }
-    false
 }
 
-fn parse_splitter_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<dyn Error>> {
+pub fn parse_splitter_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<dyn Error>> {
     // Open the file in read-only mode with buffer.
     let file = File::open(path)?;
     let reader = BufReader::new(file);
-
-    // Read the JSON contents of the file as an instance of `User`.
     let split = serde_json::from_reader(reader)?;
-
-    // Return the `User`.
     Ok(split)
-}
-
-fn parser() {
-    
-    
-
-    
-    // for definition in conf.definitions {
-    //     if definition.more.is_some() {
-    //         dbg!(&definition.name);
-    //     }
-
-    //     if definition.next.is_some() {
-    //         dbg!(&definition.name);
-    //     }
-    // }
-
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn autostart_sm_is_active() {
         let conf = parse_splitter_file("data/SuperMetroid.json").unwrap();
-        let autostart_is_set = match conf.autostart {
-            Some(i) => is_active(&i.active),
+        let autorstart_active = match conf.autostart {
+            Some(c) => c.is_active(),
             None => false
         };
-        assert!(autostart_is_set);
+        assert!(autorstart_active);
     }
 
     #[test]
     fn autostart_actraiser_is_inactive() {
         let conf = parse_splitter_file("data/actraiser.json").unwrap();
-        let autostart_is_set = match conf.autostart {
-            Some(i) => is_active(&i.active),
+        let autorstart_active = match conf.autostart {
+            Some(c) => c.is_active(),
             None => false
         };
-        assert!(!autostart_is_set);
+        assert!(!autorstart_active);
     }
-
+    
     #[test]
     fn igt_sm_is_set() {
         let conf = parse_splitter_file("data/SuperMetroid.json").unwrap();
-        let igt_is_set = match conf.igt {
-            Some(i) => is_active(&i.active),
-            None => false
-        };
-        assert!(igt_is_set);
-    
+        assert!(conf.igt.is_some());
     }
+
+    #[test]
+    fn igt_alttp_is_not_set() {
+        let conf = parse_splitter_file("data/alttp-subplits.json").unwrap();
+        dbg!(&conf.igt);
+        let igt_is_set = match conf.igt {
+            Some(i) => i.is_active(),
+            None => false,
+        };
+        assert!(!igt_is_set);
+    }
+
+
 }
